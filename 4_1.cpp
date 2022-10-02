626522b45933bfc35e2f47d95d036882c0ae052d
Ilnur Valeev <39234087+ilnurvaleev@users.noreply.github.com>
2022-10-02 17:35:40 +0300

    Create 4_1.cpp

---
+++ 4_1.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+vector<int> parent;
+vector<int> w;
+int MaxW = 0;
+
+
+ll get(ll node){
+    if(parent[node] == node)
+        return node;
+    parent[node] = get(parent[node]);
+    return parent[node];
+}
+
+void unionSet(ll x, ll y){
+    x = get(x);
+    y = get(y);
+
+    if(x == y) return;
+
+//    для задачи 3_3 это бесполезно
+////    после этого if-а вес x будет больше чем у y
+//    if(w[x] < w[y])
+//        swap(x, y);
+
+    w[x] += w[y];
+    parent[y] = x;
+
+    MaxW = max(MaxW, w[x]);
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    parent.assign(n, 0);
+    w.assign(n, 1);
+
+    for(int i = 0; i < n; ++i){
+        int x;
+        cin >> x;
+
+        w[i] = x;
+        parent[i] = i;
+
+        MaxW = max(MaxW, x);
+    }
+
+    for(int i = 0; i < m; ++i){
+         int x, y;
+         cin >> x >> y;
+         --x, --y;
+
+        unionSet(x, y);
+        cout << MaxW << endl;
+    }
+    return 0;
+}
